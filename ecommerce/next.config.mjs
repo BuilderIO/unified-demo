0aca260f5d9b36d09418c486baf79fbadcdc31fe
Tim Garibaldi <tgaribal@gmail.com>
2024-07-16 14:26:43 -0700

    remove hydration overlay for now to reintroduce devtools

--- ecommerce/next.config.mjs
+++ ecommerce/next.config.mjs
@@ -11,12 +11,14 @@ const nextConfig = BuilderDevTools()({
   },
 });
 
-const configWithOverlay = withHydrationOverlay({
-  /**
-   * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
-   * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
-   */
-  appRootSelector: "main",
-})(nextConfig);
+export default nextConfig;
 
-export default configWithOverlay;
+// const configWithOverlay = withHydrationOverlay({
+//   /**
+//    * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
+//    * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
+//    */
+//   appRootSelector: "main",
+// })(nextConfig);
+
+// export default configWithOverlay;
