a8df03e409c9afb2d183bd84d1cc793a364b4e15
Steve Sewell <steve@builder.io>
2024-09-26 09:49:11 -0500

    whatever

--- ecommerce/components/builder.tsx
+++ ecommerce/components/builder.tsx
@@ -1,6 +1,11 @@
-"use client"
+"use client";
 import { ComponentProps } from "react";
-import { BuilderComponent, Builder, builder, useIsPreviewing } from "@builder.io/react";
+import {
+  BuilderComponent,
+  Builder,
+  builder,
+  useIsPreviewing,
+} from "@builder.io/react";
 import DefaultErrorPage from "next/error";
 import "../builder-registry";
 
@@ -11,7 +16,8 @@ type BuilderPageProps = ComponentProps<typeof BuilderComponent>;
 export function RenderBuilderContent(props: BuilderPageProps) {
   // Call the useIsPreviewing hook to determine if
   // the page is being previewed in Builder
-  const isPreviewing = useIsPreviewing();///(Builder.isPreviewing || Builder.isEditing)
+  const isPreviewing = useIsPreviewing(); ///(Builder.isPreviewing || Builder.isEditing)
+
   // If "content" has a value or the page is being previewed in Builder,
   // render the BuilderComponent with the specified content and model props.
   if (props.content || isPreviewing) {
