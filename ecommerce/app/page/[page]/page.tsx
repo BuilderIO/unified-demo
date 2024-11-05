c64c6ab69d31eb0f7faeb86b97c8bfa95b508d38
Madison Dickson <madison@builder.io>
2024-11-05 15:24:34 -0700

    add a new route for free-form pages

---
+++ ecommerce/app/page/[page]/page.tsx
@@ -0,0 +1,37 @@
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from "@/src/components/builder";
+
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+interface PageProps {
+  params: {
+    page: string[];
+  };
+}
+
+// export const revalidate = 500;
+
+export default async function Page(props: PageProps) {
+  const builderModelName = "freeform-page";
+  let locale = "en-US";
+
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get(builderModelName, {
+      userAttributes: {
+        // Use the page path specified in the URL to fetch the content
+        urlPath: "/page/" + (props?.params?.page || ""),
+        locale,
+      },
+      locale
+    })
+    // Convert the result to a promise
+    .toPromise();
+
+  return (
+    <>
+      {/* Render the Builder page */}
+      <RenderBuilderContent locale={locale} content={content} model={builderModelName} options={{ enrich: true }} data={{ username: "user@builder" }} />
+    </>
+  );
+}
