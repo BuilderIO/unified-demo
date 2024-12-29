32242061056e91249b4824ad6424b787deab3112
Tim Garibaldi <tgaribal@gmail.com>
2024-12-29 14:24:11 -0800

    remove console logs

--- vcp-playground/src/components/Card/ProductCard.tsx
+++ vcp-playground/src/components/Card/ProductCard.tsx
@@ -16,12 +16,10 @@ const ProductCard: React.FC<ProductCardProps> = ({
   const [ productData, setProduct ] = useState(product);
 
   useEffect(() => {
-    console.log('PRODUCT DATA', productData, product);
     async function fetchContent() {
       await fetch("https://cdn.builder.io/api/v3/content/product-data?apiKey=a87584e551b6472fa0f0a2eb10f2c0ff&query.id=088c35a5a6914ac68b99a4ea12abba6a").then
       (async (res) => {
         const data = await res.json();
-        console.log('DATA', data.results[0]);
         setProduct(data.results[0]);
       });
       ;
