d000ec45d79064f017259e30fb6865bd2af173f4
Tim Garibaldi <tgaribal@gmail.com>
2026-06-18 15:14:27 -0700

    remove the apps we no longer use for demos

--- ecommerce/app/layout.tsx
+++ ecommerce/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "@/src/components/Layout/Footer";
 import { RenderBuilderContent } from "@/src/components/builder";
 import QueryProvider from "@/src/components/QueryProvider";
+import Link from "next/link";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
@@ -33,7 +34,24 @@ export default async function RootLayout({
           <main>
             {bannerContent && <RenderBuilderContent model="banner" content={bannerContent} data={{username: "superUser123"}} />}
             <Header headerContent={headerContent} />
-            <div className="container">{children}</div>
+            <nav style={{ display: "flex", gap: "12px", padding: "16px" }}>
+              <Link href="/first">
+                <button>First</button>
+              </Link>
+
+              <Link href="/second">
+                <button>Second</button>
+              </Link>
+
+              <Link href="/third">
+                <button>Third</button>
+              </Link>
+
+              <Link href="/fourth">
+                <button>Fourth</button>
+              </Link>
+            </nav>
+            {/* <div className="container">{children}</div> */}
             <Footer />
           </main>
         </QueryProvider>
